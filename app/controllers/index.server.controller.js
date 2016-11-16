fb0556dd06a00a1a5f3e9c79480500dfb6630471
zefta27 <zeftaadetya@gmail.com>
2016-11-17 06:47:55 +0700

    Commit Pertama

---
+++ app/controllers/index.server.controller.js
@@ -0,0 +1,11 @@
+exports.render = function (req, res) {
+  if (req.session.lastvisit) {
+    console.log(req.session.lastVisit);
+  }
+
+  req.session.lastVisit = new Date();
+
+  res.render('index',{
+    title: 'Hello World'
+  })
+};
---
+++ server.js
@@ -0,0 +1,12 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var express = require('./config/express'),
+mongoose = require('./config/mongoose'),
+passport = require('./config/passport');
+var db = mongoose();
+var app = express();
+var passport = passport();
+app.listen(3000);
+module.exports = app;
+
+console.log('Server running at http://localhost:3000/')
