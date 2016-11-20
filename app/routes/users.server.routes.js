4bfe961a039889d91332acb9199370a5dfa29f69
zefta27 <zeftaadetya@gmail.com>
2016-11-21 06:36:30 +0700

    commit-message

--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -1,14 +1,33 @@
-var users = require('../../app/controllers/users.server.controller');
+// var users = require('../../app/controllers/users.server.controller');
+//
+// module.exports = function(app){
+//   app.route('/users')
+//   .post(users.create)
+//   .get(users.list);
+//
+//   app.route('/users/:userId')
+//   .get(users.read)
+//   .put(users.update)
+//   .delete(users.delete);
+//
+//   app.param('userId', users.userByID);
+// };
 
-module.exports = function(app){
-  app.route('/users')
-  .post(users.create)
-  .get(users.list);
+ var users = require('../../app/controllers/users.server.controller'),
+  passport = require('passport');
 
-  app.route('/users/:userId')
-  .get(users.read)
-  .put(users.update)
-  .delete(users.delete);
+module.exports = function (app) {
+  app.route('/signup')
+    .get(users.renderSignUp)
+    .post(users.signup);
 
-  app.param('userId', users.userByID);
+  app.route('/signin')
+    .get(users.renderSignIn)
+    .post(passport.authenticate('local', {
+      successRedirect: '/',
+      failureRedirect: '/signin',
+      failureFlash: true
+    }));
+
+    app.get('/signout', users.signout);
 };
